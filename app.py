de5880dca2e5ef1d0f0c1f4886ecf0c9ee7a0087
AbhijitKurane <abhijit.kurane@e-zest.in>
2024-04-17 18:57:30 +0530

    Add files via upload
    init commit

---
+++ app.py
@@ -0,0 +1,8 @@
+import streamlit as st
+from utils import getdate
+
+st.title('welcome')
+st.write('date::'+getdate())
+
+
+
---
+++ utils.py
@@ -0,0 +1,4 @@
+import datetime
+
+def getdate():
+    return str(datetime.datetime.now())
